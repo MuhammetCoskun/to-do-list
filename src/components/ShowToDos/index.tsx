cac060cc975fd78f726ed0ce76e52c9a6fa5aded
Muhammet <mcoskun86@gmail.com>
2022-04-18 16:02:38 +0300

    test cases for form component

---
+++ src/components/ShowToDos/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShowToDos = (): JSX.Element => {
+  return <p>ShowToDos Component</p>;
+};
+
+export default ShowToDos;
---
+++ src/components/ToDoForm/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ToDoForm = () => {
+  return <p>ToDoForm Component</p>;
+};
+
+export default ToDoForm;
